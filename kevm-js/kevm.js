54563dde87874e4ffb57c8d9df5c046d1f823604
Markus <markus@gmtec.it>
2018-06-20 18:41:03 +0200

    using nconf to store random generated private key and deployed contract address for future executions in a separate file Please forgive and correct all grammar and syntax errors.

--- kevm-js/kevm.js
+++ kevm-js/kevm.js
@@ -1,5 +1,3 @@
-//const PRIVATEKEY = ''
-
 // IMPORTANT: after 'npm install' you must apply the following patch to node_modules/web3-eth-accounts/src/index.js
 // 179,182c179,182
 // <                 transaction.data,
@@ -24,6 +22,9 @@ const request = require('request-promise-native')
 const TARGET_ACCOUNT_BALANCE = 40000000000000000
 const FAUCET_INTERVAL = 60000  // to prevent faucet error (too many requests in given amount of time)
 
+var PRIVATEKEY;
+var CONTRACTADDRESS;
+
 process.on('unhandledRejection', err => {
   console.log(err);
 });
@@ -31,36 +32,74 @@ process.on('unhandledRejection', err => {
 const providerUrl = 'https://kevm-testnet.iohkdev.io:8546'
 //const providerUrl = 'http://localhost:3000'
 
+// ******************************
+// Step 1 - look for existing privateKey and contractAddress in local config file
+// ******************************
+
+var nconf = require('nconf');
+nconf.use('file', { file: './myData.json' });
+nconf.load();
+if (nconf.get('privateKey') != undefined ) {
+	PRIVATEKEY = nconf.get('privateKey');
+    console.log('Existing personal wallet privateKey found in myData.json');
+} else {
+    console.log('No personal wallet privateKey found in myData.json');
+}
+if (nconf.get('contractAddress') != undefined ) {
+	CONTRACTADDRESS = nconf.get('contractAddress');
+    console.log('Existing deployed contractAddress found in myData.json');
+} else {
+    console.log('No deployed contractAddress found in myData.json');
+}
+
+  
 const run = async () => {
   const web3 = new Web3(providerUrl)
 
   // ******************************
-  // Step 1 - create an account
-  // To create an account, you need a random private key. This portion of the 
-  // script will create a random key string that can simply be added to the top
-  // of the script so it can be re-run with the same account.
+  // Step 2 - try using the account, and if it fails create a new one
+  // To create an account, you need a random generated private key. 
+  // This portion of the script will try create a new random key string
+  // The privateKey is stored in local config file for further operations
   // ******************************
 
   try {
     var account = web3.eth.accounts.privateKeyToAccount(PRIVATEKEY)
   } catch (err) {
-    const rand = crypto.randomBytes(32).toString('hex')
-    console.log("No private key - generating random key.")
-    console.log("- add the following line to the top of the script and re-run:")
-    console.log("const PRIVATEKEY = '0x" + rand + "'");
-    process.exit()
-  }
-  const res = web3.eth.accounts.wallet.add(account)
+    const rand = crypto.randomBytes(32).toString('hex');
+	account = web3.eth.accounts.privateKeyToAccount("0x" + rand + "");
+	nconf.set('privateKey', '0x'+rand+'');
+	nconf.save(function (err) {
+		if (err) {
+			console.error(err.message);
+			return;
+		} else {
+			console.log("A new random PRIVATEKEY was generated and stored in ./myData.json");
+			console.log("This key will be used now and for future operations");
+		}
+	  });
+  } 
+  var res = web3.eth.accounts.wallet.add(account);
 
   // ******************************
-  // Step 2 - fund the account
+  // Step 3 - fund the account
   // If the account balance is zero, here we request test tokens from the IOHK faucet
   // and wait until the account is funded.
   // ******************************
 
-  console.log("Account = " + account.address)
+  //console.log("Account = " + account.address)
   let balance = parseInt(await web3.eth.getBalance(account.address), 10)
   console.log("Account balance = " + balance)
+  if (balance <= TARGET_ACCOUNT_BALANCE) {
+	  console.log("##########################################################################")
+	  console.log("#                                                                        #")
+	  console.log("#  while your new account will be credited with test tokens (takes 5min) #")
+	  console.log("#  please have a look at https://forum.cardano.org/c/developers          #")
+	  console.log("#  to learn new things and pass on your knowledge to others.             #")
+	  console.log("#                                                                        #")
+	  console.log("##########################################################################")
+  }
+  
   while (balance <= TARGET_ACCOUNT_BALANCE) {
     await new Promise(async (res,rej) => {
       console.log("Requesting more test tokens from faucet (waiting " + FAUCET_INTERVAL / 1000 + " seconds)")
@@ -85,69 +124,80 @@ const run = async () => {
   }
 
   // ******************************
-  // Step 3 - compile the contract
+  // Step 4 - compile the contract
   // Use the solcjs package to obtain the abi and binary for the following Solidity source
   // ******************************
-
-  console.log("Compiling contract...")
   const solc = require('solc')
   const contractSource = `
 
-    // Test Solidity Contract
-    pragma solidity ^0.4.0;
+	// Test Solidity Contract
+	pragma solidity ^0.4.0;
 
-    contract Counter {
-      int private count = 0;
-      function incrementCounter() public {
-        count += 1;
-      }
-      function decrementCounter() public {
-        count -= 1;
-      }
-      function getCount() public constant returns (int) {
-        return count;
-      }
-    }
+	contract Counter {
+	  int private count = 0;
+	  function incrementCounter() public {
+		count += 1;
+	  }
+	  function decrementCounter() public {
+		count -= 1;
+	  }
+	  function getCount() public constant returns (int) {
+		return count;
+	  }
+	}
 
-  `
+	`
   const output = solc.compile(contractSource, 1)
   const abi = output.contracts[':Counter'].interface
   const bin = output.contracts[':Counter'].bytecode
   //console.log("abi=" + abi)
   //console.log("bin=" + bin)
+  if (CONTRACTADDRESS == undefined) {
+  
+    // ******************************
+    // Step 5 - deploy the contract
+    // ******************************
+    const contract = new web3.eth.Contract(JSON.parse(abi))
+    const myContract = await contract.deploy({
+  	data: "0x" + bin
+    }).send({
+  	from: account.address,
+  	gas: 5000000,
+  	gasPrice: 5000000000
+    })
+    CONTRACTADDRESS = myContract.options.address;
+    console.log("New contract successfully compiled and deployed")
+    // Store the deployed contract address in local config file
+    nconf.set('contractAddress', CONTRACTADDRESS);
+    nconf.save(function (err) {
+  	if (err) {
+  	  console.error(err.message);
+  	  return;
+  	}
+  		console.log("The CONTRACTADDRESS "+CONTRACTADDRESS)
+  		console.log("was stored in myData.json and will be used for future operations")
+    });
+  
+  } 
 
   // ******************************
-  // Step 4 - deploy the contract
+  // Step 6 - test the deployed contract
   // ******************************
-  console.log("Deploying contract...")
-  const contract = new web3.eth.Contract(JSON.parse(abi))
-  const deployed = await contract.deploy({
-    data: "0x" + bin
-  }).send({
+
+  console.log("Now going to interact with contract " + CONTRACTADDRESS)  
+  const instance = new web3.eth.Contract(JSON.parse(abi), CONTRACTADDRESS)
+  // Test setter and getter
+  const beforeCount = await instance.methods.getCount().call()
+  console.log("Count before=" + beforeCount)
+  await instance.methods.incrementCounter().send({
     from: account.address,
-    gas: 5000000,
+    gas: 100000,
     gasPrice: 5000000000
   })
-
-  // ******************************
-  // Step 5 - test deployed contract
-  // ******************************
-  if (deployed.options.address !== undefined) {
-    console.log("Contract address=" + deployed.options.address)
-    const instance = new web3.eth.Contract(JSON.parse(abi), deployed.options.address)
-    // Test setter and getter
-    const beforeCount = await instance.methods.getCount().call()
-    console.log("Count before=" + beforeCount)
-    await instance.methods.incrementCounter().send({
-      from: account.address,
-      gas: 100000,
-      gasPrice: 5000000000
-    })
-    const afterCount = await instance.methods.getCount().call()
-    console.log("Count after=" + afterCount)
+  const afterCount = await instance.methods.getCount().call()
+  console.log("Count after=" + afterCount)
   }
 
-}
 
 try {
   run()
