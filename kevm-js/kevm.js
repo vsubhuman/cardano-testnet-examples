f92be866b6ded19684b7effa46ca45a834b00931
Markus <markus@gmtec.it>
2018-06-21 18:34:04 +0200

    added inital setup guide grouped all configurable variables together introduced CONFIGURATION variable introduced log4js for colored messages to stoud + logfile

--- kevm-js/kevm.js
+++ kevm-js/kevm.js
@@ -1,55 +1,91 @@
-// IMPORTANT: after 'npm install' you must apply the following patch to node_modules/web3-eth-accounts/src/index.js
-// 179,182c179,182
-// <                 transaction.data,
-// <                 Bytes.fromNat(transaction.chainId || "0x1"),
-// <                 "0x",
-// <                 "0x"]);
-// —-
-// >                 transaction.data]);
-// >                 //Bytes.fromNat(transaction.chainId || "0x1"),
-// >                 //"0x",
-// >                 //"0x"]);
-// 187c187,188
-// <             var signature = Account.makeSigner(Nat.toNumber(transaction.chainId || "0x1") * 2 + 35)(Hash.keccak256(rlpEncoded), privateKey);
-// —-
-// >             //var signature = Account.makeSigner(Nat.toNumber(transaction.chainId || "0x1") * 2 + 35)(Hash.keccak256(rlpEncoded), privateKey);
-// >             var signature = Account.makeSigner(27)(Hash.keccak256(rlpEncoded), privateKey);
+/*
+FIRST: copy and run the following npm install lines into your console to install all all required modules (if not allready installed)
+
+npm install web3
+npm install crypto
+npm install request-promise-native
+npm install log4js
+npm install nconf
+
+
+SECOND: after 'npm install' you must apply the following patch to node_modules/web3-eth-accounts/src/index.js
+179,182c179,182
+                 transaction.data,
+                 Bytes.fromNat(transaction.chainId || "0x1"),
+                 "0x",
+                 "0x"]);
+-
+                 transaction.data]);
+                 //Bytes.fromNat(transaction.chainId || "0x1"),
+                 //"0x",
+                 //"0x"]);
+
+187c187,188
+             var signature = Account.makeSigner(Nat.toNumber(transaction.chainId || "0x1") * 2 + 35)(Hash.keccak256(rlpEncoded), privateKey);
+-
+             //var signature = Account.makeSigner(Nat.toNumber(transaction.chainId || "0x1") * 2 + 35)(Hash.keccak256(rlpEncoded), privateKey);
+             var signature = Account.makeSigner(27)(Hash.keccak256(rlpEncoded), privateKey);
+
+*/
+
+// ******************************************************************************
+// configurable variables
+ 
+const TARGET_ACCOUNT_BALANCE = 40000000000000000
+const FAUCET_INTERVAL = 60000  // to prevent faucet error (too many requests in given amount of time)
+
+const CONFIGURATION = 'test2'  // to work with and switch between different accounts and contracts (config is stored in ./[name].json)
+
+const LOGGER_LEVEL = 'debug';   // fatal < errror < warn < info < debug
+
+const providerUrl = 'https://kevm-testnet.iohkdev.io:8546'
+//const providerUrl = 'http://localhost:3000'
+
+// ******************************************************************************
 
 const Web3 = require('web3')
 const crypto = require('crypto')
 const request = require('request-promise-native')
 
-const TARGET_ACCOUNT_BALANCE = 40000000000000000
-const FAUCET_INTERVAL = 60000  // to prevent faucet error (too many requests in given amount of time)
+
+var log4js = require('log4js');
+log4js.configure({
+  appenders: {
+    out: { type: 'stdout', layout: { type: 'pattern', pattern: '%[%p %m%]' } },
+    log: { type: 'file', filename: 'config_'+CONFIGURATION+'.log', layout: { type: 'pattern', pattern: '%d %r %p %m' } }
+  },
+  categories: {
+    default: { appenders: [ 'out', 'log' ], level: 'debug' }
+  }
+});
+var logger = log4js.getLogger();
+logger.level = LOGGER_LEVEL;
 
 var PRIVATEKEY;
 var CONTRACTADDRESS;
 
 process.on('unhandledRejection', err => {
-  console.log(err);
+  logger.error(err);
 });
-
-const providerUrl = 'https://kevm-testnet.iohkdev.io:8546'
-//const providerUrl = 'http://localhost:3000'
 
 // ******************************
 // Step 1 - look for existing privateKey and contractAddress in local config file
 // ******************************
 
 var nconf = require('nconf');
-nconf.use('file', { file: './myData.json' });
+nconf.use('file', { file: './config_'+CONFIGURATION+'.json' });
 nconf.load();
 if (nconf.get('privateKey') != undefined ) {
 	PRIVATEKEY = nconf.get('privateKey');
-    console.log('Existing personal wallet privateKey found in myData.json');
+    logger.info('Existing personal wallet privateKey found in config_'+CONFIGURATION+'.json');
 } else {
-    console.log('No personal wallet privateKey found in myData.json');
+    logger.warn('No personal wallet privateKey found in config_'+CONFIGURATION+'.json');
 }
 if (nconf.get('contractAddress') != undefined ) {
 	CONTRACTADDRESS = nconf.get('contractAddress');
-    console.log('Existing deployed contractAddress found in myData.json');
+    logger.info('Existing deployed contractAddress found in config_'+CONFIGURATION+'.json');
 } else {
-    console.log('No deployed contractAddress found in myData.json');
+    logger.warn('No deployed contractAddress found in config_'+CONFIGURATION+'.json');
 }
 
   
@@ -74,8 +110,7 @@ const run = async () => {
 			console.error(err.message);
 			return;
 		} else {
-			console.log("A new random PRIVATEKEY was generated and stored in ./myData.json");
-			console.log("This key will be used now and for future operations");
+			logger.debug('was generated and stored in ./config_'+CONFIGURATION+'.json for future operations');
 		}
 	  });
   } 
@@ -87,27 +122,27 @@ const run = async () => {
   // and wait until the account is funded.
   // ******************************
 
-  //console.log("Account = " + account.address)
+  logger.info('Account = ' + account.address)
   let balance = parseInt(await web3.eth.getBalance(account.address), 10)
-  console.log("Account balance = " + balance)
+  logger.info('Account balance = ' + balance)
   if (balance <= TARGET_ACCOUNT_BALANCE) {
-	  console.log("##########################################################################")
-	  console.log("#                                                                        #")
-	  console.log("#  while your new account will be credited with test tokens (takes 5min) #")
-	  console.log("#  please have a look at https://forum.cardano.org/c/developers          #")
-	  console.log("#  to learn new things and pass on your knowledge to others.             #")
-	  console.log("#                                                                        #")
-	  console.log("##########################################################################")
+	  logger.debug('##########################################################################')
+	  logger.debug('#                                                                        #')
+	  logger.debug('#  while your new account will be credited with test tokens (takes 5min) #')
+	  logger.debug('#  please have a look at https://forum.cardano.org/c/developers          #')
+	  logger.debug('#  to learn new things and pass on your knowledge to others.             #')
+	  logger.debug('#                                                                        #')
+	  logger.debug('##########################################################################')
   }
   
   while (balance <= TARGET_ACCOUNT_BALANCE) {
     await new Promise(async (res,rej) => {
-      console.log("Requesting more test tokens from faucet (waiting " + FAUCET_INTERVAL / 1000 + " seconds)")
+      logger.info('Requesting more test tokens from faucet (waiting ' + FAUCET_INTERVAL / 1000 + ' seconds)')
       const url = "https://kevm-testnet.iohkdev.io:8099/faucet?address=" + account.address
       try {
         await request.post(url)
       } catch (err) {
-        console.log(err.message)
+        logger.error(err.message)
         process.exit()
       }
       var funded = false
@@ -120,7 +155,7 @@ const run = async () => {
       }, FAUCET_INTERVAL)
     })
     balance = parseInt(await web3.eth.getBalance(account.address), 10)
-    console.log("Account balance = " + balance)
+    logger.info('Account balance = ' + balance)
   }
 
   // ******************************
@@ -150,8 +185,8 @@ const run = async () => {
   const output = solc.compile(contractSource, 1)
   const abi = output.contracts[':Counter'].interface
   const bin = output.contracts[':Counter'].bytecode
-  //console.log("abi=" + abi)
-  //console.log("bin=" + bin)
+  //logger.info("abi=" + abi)
+  //logger.info("bin=" + bin)
   if (CONTRACTADDRESS == undefined) {
   
     // ******************************
@@ -166,7 +201,7 @@ const run = async () => {
   	gasPrice: 5000000000
     })
     CONTRACTADDRESS = myContract.options.address;
-    console.log("New contract successfully compiled and deployed")
+    logger.info('New contract successfully compiled and deployed')
     // Store the deployed contract address in local config file
     nconf.set('contractAddress', CONTRACTADDRESS);
     nconf.save(function (err) {
@@ -174,8 +209,8 @@ const run = async () => {
   	  console.error(err.message);
   	  return;
   	}
-  		console.log("The CONTRACTADDRESS "+CONTRACTADDRESS)
-  		console.log("was stored in myData.json and will be used for future operations")
+  		logger.info('contract '+CONTRACTADDRESS)
+  		logger.debug('was stored in config_'+CONFIGURATION+'.json for future operations')
     });
   
   } 
@@ -184,24 +219,26 @@ const run = async () => {
   // Step 6 - test the deployed contract
   // ******************************
 
-  console.log("Now going to interact with contract " + CONTRACTADDRESS)  
+  logger.info('Now going to interact with contract ' + CONTRACTADDRESS)  
   const instance = new web3.eth.Contract(JSON.parse(abi), CONTRACTADDRESS)
   // Test setter and getter
   const beforeCount = await instance.methods.getCount().call()
-  console.log("Count before=" + beforeCount)
+  logger.info('Count before=' + beforeCount)
   await instance.methods.incrementCounter().send({
     from: account.address,
     gas: 100000,
     gasPrice: 5000000000
   })
   const afterCount = await instance.methods.getCount().call()
-  console.log("Count after=" + afterCount)
+  logger.info('Count after=' + afterCount)
   }
 
 
 try {
   run()
 } catch (err) {
-  console.log(err)
+  logger.error(err)
 }
+
+
 
